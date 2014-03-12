923d017f7143089622da29e5e2725e3c5f7a7b2f
Juri Berlanda <juriberlanda@hotmail.com>
2014-03-12 14:49:00 +0100

    [TtsAgent] Created project hierarchy.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

---
+++ src/org/duckdns/raven/ttscallresoponder/MainActivity.java
@@ -0,0 +1,81 @@
+package org.duckdns.raven.ttscallresoponder;
+
+import java.util.Locale;
+
+import org.duckdns.raven.ttscallresoponder.R.id;
+import org.duckdns.raven.ttscallresoponder.ttsStuff.CallTTSEngine;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private CallTTSEngine ttsEngine = null;
+	private Button btnSpeak = null;
+	private EditText txtText = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_main);
+
+		this.ttsEngine = new CallTTSEngine(this, Locale.US);
+
+		this.btnSpeak = (Button) this.findViewById(id.button_compileToTTS);
+		this.txtText = (EditText) this.findViewById(id.editText_textToCompile);
+
+		this.btnSpeak.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		this.getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		this.ttsEngine.speak(this.txtText.getText().toString());
+	}
+
+}
---
+++ src/org/duckdns/raven/ttscallresoponder/ttsStuff/CallTTSEngine.java
@@ -0,0 +1,26 @@
+package org.duckdns.raven.ttscallresoponder.ttsStuff;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+
+public class CallTTSEngine implements OnInitListener {
+	private TextToSpeech ttsEngine = null;
+
+	public CallTTSEngine(Activity parent, Locale locale) {
+		this.ttsEngine = new TextToSpeech(parent, this);
+	}
+
+	public void speak(String text) {
+		this.ttsEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+	}
+
+	@Override
+	public void onInit(int status) {
+		if (status == TextToSpeech.SUCCESS) {
+			this.ttsEngine.setLanguage(Locale.US);
+		}
+	}
+}
