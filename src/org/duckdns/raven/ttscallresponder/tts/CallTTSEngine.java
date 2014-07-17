3f3be8fee9f259465c10e641c0213e8bb63bbfc7
Juri Berlanda <juriberlanda@hotmail.com>
2014-07-17 15:14:40 +0200

    [CallTTSEngine] Added spoken feedback if not running in SteiingsActivity.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/org/duckdns/raven/ttscallresponder/tts/CallTTSEngine.java
+++ src/org/duckdns/raven/ttscallresponder/tts/CallTTSEngine.java
@@ -3,18 +3,28 @@ package org.duckdns.raven.ttscallresponder.tts;
 import java.util.Locale;
 
 import org.duckdns.raven.ttscallresponder.dataAccess.SettingsManager;
+import org.duckdns.raven.ttscallresponder.ui.settings.ActivitySettings;
 
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 
 public class CallTTSEngine implements OnInitListener {
 
+	private static String TAG = "CallTTSEngine";
+
 	private TextToSpeech ttsEngine = null;
 	private boolean isTtsEngineUp = false;
+	private boolean speakEnterExit = true;
 
 	public CallTTSEngine(Context parent) {
 		this.ttsEngine = new TextToSpeech(parent, this);
+	}
+
+	public CallTTSEngine(ActivitySettings parent) {
+		this.ttsEngine = new TextToSpeech(parent, this);
+		this.speakEnterExit = false;
 	}
 
 	public void speak(String text) {
@@ -25,7 +35,13 @@ public class CallTTSEngine implements OnInitListener {
 	public void onInit(int status) {
 		if (status == TextToSpeech.SUCCESS) {
 			this.isTtsEngineUp = true;
+			Log.i(CallTTSEngine.TAG, "TTS started - parameterizing");
 			this.parameterizeTtsEngine();
+			Log.i(CallTTSEngine.TAG, "TTS parameterized");
+			if (this.speakEnterExit)
+				this.speak("T T S service is running");
+			else
+				Log.i(CallTTSEngine.TAG, "Settings mode - not speaking on enter");
 		}
 	}
 
@@ -49,6 +65,16 @@ public class CallTTSEngine implements OnInitListener {
 
 	public void stopEngine() {
 		this.isTtsEngineUp = false;
+		if (this.speakEnterExit) {
+			this.speak("Stopping T T S service");
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else
+			Log.i(CallTTSEngine.TAG, "Settings mode - not speaking on exit");
+
 		this.ttsEngine.shutdown();
 	}
 }
