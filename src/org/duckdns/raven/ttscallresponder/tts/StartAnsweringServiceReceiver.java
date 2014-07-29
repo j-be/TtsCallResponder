4c43abf9a243ef1ff682af6bcb6a670984a61219
Juri Berlanda <juriberlanda@hotmail.com>
2014-07-29 13:41:27 +0200

    [CallResponder] Add calls to list of answered calls.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/org/duckdns/raven/ttscallresponder/tts/StartAnsweringServiceReceiver.java
+++ src/org/duckdns/raven/ttscallresponder/tts/StartAnsweringServiceReceiver.java
@@ -1,5 +1,8 @@
 package org.duckdns.raven.ttscallresponder.tts;
 
+import org.duckdns.raven.ttscallresponder.domain.call.Call;
+import org.duckdns.raven.ttscallresponder.domain.call.PersistentCallList;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -19,10 +22,11 @@ public class StartAnsweringServiceReceiver extends BroadcastReceiver {
 			Intent startResponderService = new Intent(context, TtsAnsweringService.class);
 			context.startService(startResponderService);
 			Log.i(TAG, "Service started");
-		}
 
-		if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-			Log.i(TAG, "InCall - answering");
+			Log.i(TAG, "Adding to answered calls: " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+			PersistentCallList callList = new PersistentCallList(context.getFilesDir());
+			callList.add(new Call(intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)));
+			callList.savePersistentList();
 		}
 	}
 }
