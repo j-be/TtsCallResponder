2dc9fc3b4756e8855930dd16c1eff1c9673aec74
Juri Berlanda <juriberlanda@hotmail.com>
2014-07-29 18:14:01 +0200

    [Cleanup] Restructered CallListAdapter to match to other Adapters.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/CallListAdapter.java
+++ src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/CallListAdapter.java
@@ -55,38 +55,38 @@ public class CallListAdapter extends ArrayAdapter<Call> {
 		if (convertView == null) {
 			// Inflate the layout
 			convertView = this.parent.getLayoutInflater().inflate(R.layout.widget_call, parent, false);
-
-			// Gain access to the UI elements
-			TextView caller = (TextView) convertView.findViewById(R.id.label_caller);
-			TextView callTime = (TextView) convertView.findViewById(R.id.label_callTime);
-			ImageButton callBack = (ImageButton) convertView.findViewById(R.id.button_callBack);
-
-			// Get the call for the current position
-			Call call = this.getItem(position);
-			// Resolve phone number to name
-			caller.setText(this.phoneBookAccess.getNameForPhoneNumber(call.getCaller()));
-			// FIXME: Make locale dependend
-			callTime.setText(call.getCallTime().format("%b %d %Y, %H:%M"));
-
-			// Attach the phone number to the call-back button
-			callBack.setTag(call.getCaller());
-			// Add listener to the call-back button
-			callBack.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					try {
-						// Open the dialer and pre-dial the number
-						Intent callIntent = new Intent(Intent.ACTION_DIAL);
-						callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-						callIntent.setData(Uri.parse("tel:" + v.getTag()));
-						v.getContext().startActivity(callIntent);
-					} catch (ActivityNotFoundException activityException) {
-						Log.e(CallListAdapter.TAG, "Dial failed", activityException);
-					}
-
-				}
-			});
 		}
+
+		// Gain access to the UI elements
+		TextView caller = (TextView) convertView.findViewById(R.id.label_caller);
+		TextView callTime = (TextView) convertView.findViewById(R.id.label_callTime);
+		ImageButton callBack = (ImageButton) convertView.findViewById(R.id.button_callBack);
+
+		// Get the call for the current position
+		Call call = this.getItem(position);
+		// Resolve phone number to name
+		caller.setText(this.phoneBookAccess.getNameForPhoneNumber(call.getCaller()));
+		// FIXME: Make locale dependend
+		callTime.setText(call.getCallTime().format("%b %d %Y, %H:%M"));
+
+		// Attach the phone number to the call-back button
+		callBack.setTag(call.getCaller());
+		// Add listener to the call-back button
+		callBack.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					// Open the dialer and pre-dial the number
+					Intent callIntent = new Intent(Intent.ACTION_DIAL);
+					callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					callIntent.setData(Uri.parse("tel:" + v.getTag()));
+					v.getContext().startActivity(callIntent);
+				} catch (ActivityNotFoundException activityException) {
+					Log.e(CallListAdapter.TAG, "Dial failed", activityException);
+				}
+
+			}
+		});
 
 		return convertView;
 	}
