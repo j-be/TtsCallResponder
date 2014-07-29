955c84625ace1d0347c278bdacd4b93b4647241c
Juri Berlanda <juriberlanda@hotmail.com>
2014-07-29 17:31:14 +0200

    [Doc] Commented ui.answeredCalls package.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/ActivityAnsweredCallList.java
+++ src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/ActivityAnsweredCallList.java
@@ -10,55 +10,32 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+/**
+ * This {@link Activity} provides a list of answered calls to the user.
+ * 
+ * FIXME: License
+ * 
+ * @author Juri Berlanda
+ * 
+ */
 public class ActivityAnsweredCallList extends Activity {
-
 	private static final String TAG = "ActivityAnsweredCallList";
 
+	// List of answered calls
 	private PersistentCallList answeredCallList = null;
+	// Adapter from list to ListView
 	private CallListAdapter adapter = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		Log.i(ActivityAnsweredCallList.TAG, "Enter on Create");
-
-		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.activity_answered_call_list);
-
-		this.overridePendingTransition(R.animator.anim_slide_in_from_left, R.animator.anim_slide_out_to_right);
-
-		ListView answeredCallsListView = (ListView) this.findViewById(R.id.list_answered_calls);
-		this.answeredCallList = new PersistentCallList(this.getFilesDir());
-		this.adapter = new CallListAdapter(this, this.answeredCallList.getPersistentList());
-		answeredCallsListView.setAdapter(this.adapter);
-		this.adapter.notifyDataSetChanged();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		this.overridePendingTransition(R.animator.anim_slide_in_from_right, R.animator.anim_slide_out_to_left);
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		if (this.adapter != null)
-			this.adapter.notifyDataSetChanged();
-	}
-
+	/* Add clear button to ActionBar */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		this.getMenuInflater().inflate(R.menu.activity_answered_call_list, menu);
 		return true;
 	}
 
+	/* Handle clear buttons in ActionBar */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		switch (id) {
 		case R.id.action_clearList:
@@ -72,9 +49,46 @@ public class ActivityAnsweredCallList extends Activity {
 		}
 	}
 
+	/* Clear the list */
 	public void clearAnsweredCallListClick() {
 		this.answeredCallList.clear();
 		this.answeredCallList.savePersistentList();
 		this.adapter.notifyDataSetChanged();
+	}
+
+	/* ----- Lifecycle ----- */
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.i(ActivityAnsweredCallList.TAG, "Enter on Create");
+		super.onCreate(savedInstanceState);
+
+		// Inflate layout
+		this.setContentView(R.layout.activity_answered_call_list);
+		// Set enter animation
+		this.overridePendingTransition(R.animator.anim_slide_in_from_left, R.animator.anim_slide_out_to_right);
+
+		// Create the adapter and apply it on he ListView
+		ListView answeredCallsListView = (ListView) this.findViewById(R.id.list_answered_calls);
+		this.answeredCallList = new PersistentCallList(this.getFilesDir());
+		this.adapter = new CallListAdapter(this, this.answeredCallList.getPersistentList());
+		answeredCallsListView.setAdapter(this.adapter);
+		this.adapter.notifyDataSetChanged();
+	}
+
+	/* Set exit animation when the activity becomes hidden */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		this.overridePendingTransition(R.animator.anim_slide_in_from_right, R.animator.anim_slide_out_to_left);
+	}
+
+	/* Update the list whenever the Activity is shown after being hidden */
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (this.adapter != null)
+			this.adapter.notifyDataSetChanged();
 	}
 }
--- src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/CallListAdapter.java
+++ src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/CallListAdapter.java
@@ -16,14 +16,34 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
+/**
+ * {@link ArrayAdapter} for adapting a {@link List} of {@link Call}s to a
+ * {@link ListView}. This is used in {@link ActivityAnsweredCallList}.
+ * 
+ * FIXME: License
+ * 
+ * @author Juri Berlanda
+ * 
+ */
 public class CallListAdapter extends ArrayAdapter<Call> {
 	private static final String TAG = "CallListAdapter";
 
+	// Provides phone-number to name resolving
 	private final PhoneBookAccess phoneBookAccess;
+	// Provides context needed for layout inflater
 	private final Activity parent;
 
+	/**
+	 * Default constructor.
+	 * 
+	 * @param parent
+	 *            the activity on which the list shall be drawn
+	 * @param list
+	 *            the {@link List} of {@link Call}s which shall be drawn
+	 */
 	public CallListAdapter(Activity parent, List<Call> list) {
 		super(parent, R.layout.widget_call, list);
 		this.phoneBookAccess = new PhoneBookAccess(parent);
@@ -33,22 +53,29 @@ public class CallListAdapter extends ArrayAdapter<Call> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		if (convertView == null) {
+			// Inflate the layout
 			convertView = this.parent.getLayoutInflater().inflate(R.layout.widget_call, parent, false);
 
+			// Gain access to the UI elements
 			TextView caller = (TextView) convertView.findViewById(R.id.label_caller);
 			TextView callTime = (TextView) convertView.findViewById(R.id.label_callTime);
 			ImageButton callBack = (ImageButton) convertView.findViewById(R.id.button_callBack);
 
+			// Get the call for the current position
 			Call call = this.getItem(position);
+			// Resolve phone number to name
 			caller.setText(this.phoneBookAccess.getNameForPhoneNumber(call.getCaller()));
+			// FIXME: Make locale dependend
 			callTime.setText(call.getCallTime().format("%b %d %Y, %H:%M"));
 
+			// Attach the phone number to the call-back button
 			callBack.setTag(call.getCaller());
-
+			// Add listener to the call-back button
 			callBack.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					try {
+						// Open the dialer and pre-dial the number
 						Intent callIntent = new Intent(Intent.ACTION_DIAL);
 						callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 						callIntent.setData(Uri.parse("tel:" + v.getTag()));
