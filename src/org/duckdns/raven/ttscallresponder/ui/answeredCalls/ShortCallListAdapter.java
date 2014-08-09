e3e9fcb5480e1ccbee701caa3711d49e2d4f5b58
Juri Berlanda <juriberlanda@hotmail.com>
2014-08-09 21:59:54 +0200

    [Layout] Moved button for complete list into list.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/ShortCallListAdapter.java
+++ src/org/duckdns/raven/ttscallresponder/ui/answeredCalls/ShortCallListAdapter.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.duckdns.raven.ttscallresponder.domain.call.Call;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class ShortCallListAdapter extends CallListAdapter {
 	private static final String TAG = "ShortCallListAdapter";
@@ -29,11 +31,24 @@ public class ShortCallListAdapter extends CallListAdapter {
 		Log.d(ShortCallListAdapter.TAG, "Asking for position: " + position + " of max. "
 				+ ShortCallListAdapter.MAX_DISPLAYED_ENTRIES);
 		if ((position) == ShortCallListAdapter.MAX_DISPLAYED_ENTRIES) {
-			TextView andMore = new TextView(this.parent);
+			Button andMore = new Button(this.parent);
 			andMore.setText("And " + (super.getCount() - position) + " more...");
+			andMore.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					ShortCallListAdapter.this.onShowAnsweredCallListClick(v);
+				}
+			});
 			Log.d(ShortCallListAdapter.TAG, "And more handled " + position);
 			return andMore;
 		} else
 			return super.getView(position, convertView, parent);
+	}
+
+	/* Switch to list of answered calls */
+	public void onShowAnsweredCallListClick(View view) {
+		Intent switchToCallList = new Intent(this.parent, ActivityAnsweredCallList.class);
+		switchToCallList.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		this.parent.startActivity(switchToCallList);
 	}
 }
--- src/org/duckdns/raven/ttscallresponder/ui/fragments/AnsweredCallsFragment.java
+++ src/org/duckdns/raven/ttscallresponder/ui/fragments/AnsweredCallsFragment.java
@@ -1,19 +1,15 @@
 package org.duckdns.raven.ttscallresponder.ui.fragments;
 
 import org.duckdns.raven.ttscallresponder.R;
-import org.duckdns.raven.ttscallresponder.domain.call.Call;
 import org.duckdns.raven.ttscallresponder.domain.call.PersistentCallList;
-import org.duckdns.raven.ttscallresponder.ui.answeredCalls.ActivityAnsweredCallList;
 import org.duckdns.raven.ttscallresponder.ui.answeredCalls.ShortCallListAdapter;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
@@ -31,13 +27,6 @@ public class AnsweredCallsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View ret = inflater.inflate(R.layout.fragment_answered_calls, container, false);
 
-		ret.findViewById(R.id.button_showAnsweredCallList).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				AnsweredCallsFragment.this.onShowAnsweredCallListClick(v);
-			}
-		});
-
 		this.callersList = ((ListView) ret.findViewById(R.id.list_answered_calls));
 
 		return ret;
@@ -51,13 +40,6 @@ public class AnsweredCallsFragment extends Fragment {
 	}
 
 	/* ----- User interactions ----- */
-
-	/* Switch to list of answered calls */
-	public void onShowAnsweredCallListClick(View view) {
-		Intent switchToCallList = new Intent(this.getActivity(), ActivityAnsweredCallList.class);
-		switchToCallList.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		this.startActivity(switchToCallList);
-	}
 
 	/* ----- Update UI helpers ----- */
 
