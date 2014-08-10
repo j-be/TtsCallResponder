0df9e58f4b2f19913c6bf628e4445d918bb344e8
Juri Berlanda <juriberlanda@hotmail.com>
2014-08-10 15:06:24 +0200

    [Layout] Added current event information to ResponseTemplateFragment.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/org/duckdns/raven/ttscallresponder/ui/fragments/ResponseTemplateFragmentSmall.java
+++ src/org/duckdns/raven/ttscallresponder/ui/fragments/ResponseTemplateFragmentSmall.java
@@ -1,14 +1,19 @@
 package org.duckdns.raven.ttscallresponder.ui.fragments;
 
+import java.util.Date;
+
 import org.duckdns.raven.ttscallresponder.R;
+import org.duckdns.raven.ttscallresponder.dataAccess.CalendarAccess;
 import org.duckdns.raven.ttscallresponder.domain.responseTemplate.PersistentResponseTemplateList;
 import org.duckdns.raven.ttscallresponder.domain.responseTemplate.ResponseTemplate;
+import org.duckdns.raven.ttscallresponder.domain.userData.TtsParameterCalendarEvent;
 import org.duckdns.raven.ttscallresponder.ui.responseTemplates.ActivityResponseTemplateList;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +28,8 @@ public class ResponseTemplateFragmentSmall extends Fragment {
 
 	// UI elements
 	private TextView currentResponseTemplateTitle = null;
+	private TextView currentEventTitle = null;
+	private TextView currentEventEnd = null;
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -38,6 +45,8 @@ public class ResponseTemplateFragmentSmall extends Fragment {
 
 		// Get access to UI elements
 		this.currentResponseTemplateTitle = (TextView) ret.findViewById(R.id.textView_currentResponseTemplateTitle);
+		this.currentEventTitle = (TextView) ret.findViewById(R.id.textView_currentEventTitle);
+		this.currentEventEnd = (TextView) ret.findViewById(R.id.textView_currentEventEnd);
 		ret.findViewById(R.id.textView_headerResponseTemplates).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -53,19 +62,35 @@ public class ResponseTemplateFragmentSmall extends Fragment {
 		Log.i(ResponseTemplateFragmentSmall.TAG, "Enter onResume");
 		super.onResume();
 
-		String currentTitle = null;
+		String strCurrentTemplateTitle = "<None>";
+		TtsParameterCalendarEvent currentEvent = null;
+		String strCurrentEventTitle = "<None>";
+		String strCurrentEventEnd = "<unknown>";
+		CalendarAccess calendarAccess = new CalendarAccess(this.getActivity());
 
 		// Retrieve data
 		this.responseTemplateList.loadPersistentList();
 		ResponseTemplate currentResponseTemplate = this.responseTemplateList.getCurrentResponseTemplate();
 
 		// Initialize UI elements
-		if (currentResponseTemplate == null) {
+		if (currentResponseTemplate == null)
 			Log.d(ResponseTemplateFragmentSmall.TAG, "No current response set");
-			currentTitle = "<None>";
-		} else
-			currentTitle = currentResponseTemplate.getTitle();
-		this.currentResponseTemplateTitle.setText(currentTitle);
+		else {
+			// Get current response template
+			strCurrentTemplateTitle = currentResponseTemplate.getTitle();
+			// Get ongoing event for selected calendar
+			currentEvent = calendarAccess.getCurrentEventFromCalendar(currentResponseTemplate.getCalendarId());
+			if (currentEvent != null) {
+				strCurrentEventTitle = currentEvent.getTitle();
+				strCurrentEventEnd = DateFormat.getTimeFormat(this.getActivity()).format(
+						new Date(currentEvent.getEndTime()));
+			}
+		}
+
+		// Apply data to fragment
+		this.currentResponseTemplateTitle.setText(strCurrentTemplateTitle);
+		this.currentEventTitle.setText(strCurrentEventTitle);
+		this.currentEventEnd.setText(strCurrentEventEnd);
 	}
 
 	/* Switch to response template management */
